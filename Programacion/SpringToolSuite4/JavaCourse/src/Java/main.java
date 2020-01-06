4b0b76205ebda1558e8d8ca818193e2dd6a0ea9b
tonton <japc18400@gmail.com>
2020-01-06 17:38:10 -0500

    Commit inicio proyecto Java

---
+++ Programacion/SpringToolSuite4/JavaCourse/src/Java/main.java
@@ -0,0 +1,20 @@
+package Java;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		persona persona= new persona("juan",5000,false);
+		
+		System.out.println("El nombre de la persona es: "+persona.getNombre());
+		
+		persona.setNombre("Jhonnatan");
+		
+		System.out.println("El nombre de la persona es: "+persona.getNombre());
+		System.out.println("el sueldo de la persona es: "+persona.getSueldo());
+		System.out.println("la Persona fue borrada del sistema? "+persona.isEliminar());
+		
+
+	}
+
+}
---
+++ Programacion/SpringToolSuite4/JavaCourse/src/Java/persona.java
@@ -0,0 +1,39 @@
+package Java;
+
+public class persona {
+	
+	private String nombre;
+	private double sueldo;
+	private boolean eliminado;
+	
+	public persona(String nombre,double sueldo, boolean eliminado) {
+		this.nombre=nombre;
+		this.sueldo=sueldo;
+		this.eliminado=eliminado;
+	}
+	
+	//get: obtener set:modificar
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public double getSueldo() {
+		return sueldo;
+	}
+	
+	public void setSueldo(double sueldo) {
+		this.sueldo=sueldo;
+	}
+	
+	public boolean isEliminar() {
+		return eliminado;
+	}
+	
+	public void setEliminar(boolean eliminado) {
+		this.eliminado=eliminado;
+	}
+}
