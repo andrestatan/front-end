4184f0e503552b4818c3d28f0ba4ec9032901725
andrestatan <japc18400@gmail.com>
2020-03-09 23:21:16 -0500

    calculadora app

--- calculadora/src/app/operadores/operadores.component.ts
+++ calculadora/src/app/operadores/operadores.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { from } from 'rxjs';
+import { NumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-operadores',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operadores.component.css']
 })
 export class OperadoresComponent implements OnInit {
-
+  @Input()  numero1:number;
+  @Input() numero2:number;
+  resultado:number;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  onsuma(){
+    this.resultado=this.numero1+this.numero2;
+  }
+  onresta(){
+    this.resultado=this.numero1+this.numero2;
+  }
+  onMulti(){
+    this.resultado=this.numero1+this.numero2;
+  }
+  onDivision(){
+    this.resultado=this.numero1+this.numero2;
+  }
 }
