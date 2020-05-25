c3967a5729de6832211bc1dda1effbb37e9cdb57
Jhonnatan Posada <andresposada@worldofficeonline.com>
2020-05-25 00:17:48 -0500

    adicion

--- proyectoFinal/src/app/bienvenido/bienvenido.component.ts
+++ proyectoFinal/src/app/bienvenido/bienvenido.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.component.html',
-  styleUrls: ['./bienvenido.component.css']
-})
+  styleUrls: ['./bienvenido.component.css','../app.component.css']
+}
+
 export class BienvenidoComponent implements OnInit {
+  titulo="Introduccion general";
 
   constructor() { }
 
