0ef8a351762b64911ff552af2501186bf10f8638
Jhonnatan <andresposada@worldofficeonline.com>
2020-08-30 18:22:13 -0500

    avance app curso

--- advanceCourse/src/app/app.module.ts
+++ advanceCourse/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AjustesComponent } from './ajustes/ajustes.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MensajesComponent } from './mensajes/mensajes.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { NoticiaCompletaComponent } from './noticia-completa/noticia-completa.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjustesComponent,
+    InicioComponent,
+    MensajesComponent,
+    NoticiasComponent,
+    NoticiaCompletaComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
