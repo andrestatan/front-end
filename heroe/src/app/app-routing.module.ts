1fb9abc7919c01085789bb7e50b24b082faa260c
Jhonnatan <andresposada@worldofficeonline.com>
2020-10-08 19:28:42 -0500

    adelanto aplicacion Heroes

--- heroe/src/app/app-routing.module.ts
+++ heroe/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HereComponent } from './here/here.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  {path: 'Heroe', component: HereComponent},
+  {path: 'About', component: AboutComponent},
+
+  {path: '**', pathMatch:'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- heroe/src/app/app.module.ts
+++ heroe/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HereComponent } from './here/here.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HereComponent
   ],
   imports: [
     BrowserModule,
