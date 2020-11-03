46412003dac575d853196a8f63aa01f17dc0ab44
Jhonnatan <andresposada@worldofficeonline.com>
2020-11-02 19:35:52 -0500

    commit proyecto nopviembre

--- miniCursoJS/PaletaColores/javascript/plaetacolores.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('color').addEventListener('input', getColor)
-
-function getColor(){
-let col = document.getElementById('color').value;
-
-document.getElementById('visualizar').style.background = col;
-document.getElementById('visualizar').innerHTML= col;
-}
--- miniCursoJS/miniIDE/javascript/ide.js
+++
@@ -1,7 +0,0 @@
-function visualizacion(){
-    const campoTexto=document.getElementById("areaTexto");
-    const campoVista= document.getElementById('vista');
-    let texto= campoTexto.value;
-
-    campoVista.srcdoc= texto
-}
