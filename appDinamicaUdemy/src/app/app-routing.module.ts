10df97866546760243311ff6c8600aa0d21f2f4e
Jhonnatan <andresposada@worldofficeonline.com>
2020-11-09 06:28:59 -0500

    update practice

--- appDinamicaUdemy/src/app/app-routing.module.ts
+++ appDinamicaUdemy/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { PathLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AjustesComponent } from './ajustes/ajustes.component';
+import { IncicioComponent } from './incicio/incicio.component';
+import { MensajeComponent } from './mensaje/mensaje.component';
+import { NoticiaCompletaComponent } from './noticia-completa/noticia-completa.component';
+import { NoticiaComponent } from './noticia/noticia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Inicio", component:IncicioComponent},
+  {path:"Noticias", component:NoticiaComponent},
+  {path:"Mensajes", component:MensajeComponent},
+  {path:"Noticia", component:NoticiaCompletaComponent},
+  {path:"Ajustes", component:AjustesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- appDinamicaUdemy/src/app/app.module.ts
+++ appDinamicaUdemy/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiaComponent } from './noticia/noticia.component';
+import { NoticiaCompletaComponent } from './noticia-completa/noticia-completa.component';
+import { MensajeComponent } from './mensaje/mensaje.component';
+import { IncicioComponent } from './incicio/incicio.component';
+import { AjustesComponent } from './ajustes/ajustes.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiaComponent,
+    NoticiaCompletaComponent,
+    MensajeComponent,
+    IncicioComponent,
+    AjustesComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
