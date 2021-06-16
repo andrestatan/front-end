77e7342ff4ff1a9cbdd3e6e00df2ae9b6537ef94
Jhonnatan <andresposada@worldofficeonline.com>
2021-06-15 21:18:12 -0500

    Inicio Aprendizaje React

---
+++ React/first-app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+
+
