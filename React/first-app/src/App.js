7364b01fbf73a99414950efe3218d434e016d250
Jhonnatan <andresposada@worldofficeonline.com>
2021-07-10 16:47:26 -0500

    actualizacion proyectos personales

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+
+  const handleEmailChanges = ({target : {value}}) => {
+    setemail(value)
+  }
+
+  const handlePasswordChanges = ({target : {value}}) => {
+    setpassword(value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    window.alert(`El usuario ingresado es ${email}, con contraseña ${password}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <h2> Iniciar Sesion </h2>
+        <label>
+          Correo
+          <input type="email" value={email} onChange = {handleEmailChanges}></input>
+        </label>
+        <label>
+          Contraseña
+          <input type="password" value={password} onChange = {handlePasswordChanges}></input>
+        </label>
+        <button type= "submit">
+          Enviar
+        </button>
+      </form>
+
     </div>
   );
 }
